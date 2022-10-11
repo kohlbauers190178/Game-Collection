0851c682bdbe18d81f20cc0eee8db0f4b431300b
kohlbauers190178 <kohlbauers190178@sus.htl-grieskirchen.at>
2022-10-11 09:16:26 +0200

    Project with TicTacToe and PPS-Test renamed to Game-Collection

---
+++ Game-Collection/app/src/main/java/net/htlgkr/kohlbauers190178/gamecollection/MainActivity.java
@@ -0,0 +1,58 @@
+package net.htlgkr.kohlbauers190178.gamecollection;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStartTicTacToeGame);
+        btnStart.setOnClickListener(this);
+        findViewById(R.id.btnStartPPSTestGame).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button btnClicked = findViewById(view.getId());
+
+
+        if (btnClicked.getId() == R.id.btnStartTicTacToeGame) {
+            startTicTacToeGame();
+        } else if (btnClicked.getId() == R.id.btnStartPPSTestGame) {
+            startPPSGame();
+        } else {
+            Toast.makeText(this, "Error choosing game", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        findViewById(R.id.btnStartTicTacToeGame).setVisibility(View.GONE);
+        findViewById(R.id.btnStartPPSTestGame).setVisibility(View.GONE);
+    }
+
+    private void startPPSGame() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.constraintLayout, PressPerSecondTestFragment.newInstance("", ""), "ReactionTesterFragment")
+                .addToBackStack(null)
+                .commit();
+    }
+
+
+    public void startTicTacToeGame() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.constraintLayout, TicTacToeFragment.newInstance("", ""), "TicTacToeFragment")
+                .addToBackStack(null)
+                .commit();
+    }
+}
